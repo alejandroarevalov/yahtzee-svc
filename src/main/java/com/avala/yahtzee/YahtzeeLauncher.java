d7a0da5103bbbbe8c139f59553d64ce9d124b79c
Alejandro Arévalo <alejandroarevalov@gmail.com>
2020-03-23 22:31:04 -0500

    CORS requests handled - Lvl 1

--- src/main/java/com/avala/yahtzee/YahtzeeLauncher.java
+++ src/main/java/com/avala/yahtzee/YahtzeeLauncher.java
@@ -3,8 +3,6 @@ package com.avala.yahtzee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -12,15 +10,6 @@ public class YahtzeeLauncher {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YahtzeeLauncher.class, args);
-	}
-
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("${yahtzee.cors.url.allowed}");
-			}
-		};
 	}
 
 }
--- src/main/java/com/avala/yahtzee/config/AppConfig.java
+++ src/main/java/com/avala/yahtzee/config/AppConfig.java
@@ -4,8 +4,12 @@ import com.avala.yahtzee.service.YahtzeeService;
 import com.avala.yahtzee.service.YahtzeeServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class AppConfig {
 
     @Bean
